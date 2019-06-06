731c2ffb12680cdb93488790220b68f6ce3e19a5
itzsaik <saikarthik.karunakar@gmail.com>
2019-06-06 19:24:12 +0530

    update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <h1>Github to AWS Beanstalk </h1>
   
-<h1>Introduced QA Stage into the code pipeline - 6 Jun 2019 </h1>
+<h1> QA Stage into the code pipeline - 6 Jun 2019 </h1>
 
 <?php
 echo "version :Ver 1.0";
