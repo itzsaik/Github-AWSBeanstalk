976ff3a8c258cdaff4d5d9213fd7dfbc110629f6
itzsaik <saikarthik.karunakar@gmail.com>
2019-06-06 19:15:19 +0530

    aws beanstalk

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
 <body>
 
-<h1>Saikarthik - Devops </h1>
+<h1> Devops </h1>
 
 <h1>Github to AWS Beanstalk </h1>
   
-<h1>Introduced QA Stage into the code pipeline </h1>
+<h1>Introduced QA Stage into the code pipeline - 6 Jun 2019</h1>
 
 <?php
 echo "version :Ver 1.0";
